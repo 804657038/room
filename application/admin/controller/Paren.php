a6ab4a1b437007a1b0c910dec50831e742690e13
libinghe <804657038@qq.com>
2018-07-11 10:00:15 +0800

    3

---
+++ application/admin/controller/Paren.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+
+class Paren extends Controller{
+    public function adminAutho($autho){
+//        $arr=explode(',',USER['auth']);
+//        if(!in_array($autho,$arr)){
+//            $this->result('',0,'您没有权限','json');
+//        }
+    }
+}
+?>
--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -2,14 +2,13 @@
 
 namespace app\admin\controller;
 
-use think\Controller;
 use think\Request;
 use app\admin\model\TestModel;
 /**
  * @title 测试功能
  * @description ...
  */
-class Test extends Controller
+class Test extends Paren
 {
     /**
      * @title 内容添加
@@ -22,6 +21,7 @@ class Test extends Controller
      */
     public function addtest(Request $request,TestModel $model){
 
+//        self::adminAutho('test_add'); //权限验证，先注释
         $param=$request->param();
         $result=$model->setSave($param);
         $this->result('',$result['code'],$result['msg'],'json');
